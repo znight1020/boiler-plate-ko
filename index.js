2ef25c35b90315fe0b1213d61546c623af6e3c1a
현수 <104980470+znight1020@users.noreply.github.com>
2022-10-08 07:53:50 +0900

    auth기능(페이지가 사용자를 기억 및 인증)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
     //회원 가입할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
 
@@ -39,7 +39,7 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
     // 요청된 이메일을 데이터베이스에 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -70,6 +70,23 @@ app.post("/login", (req, res) => {
                 .status(200) // 200은 성공했다는 뜻
                 .json({ loginSuccess: true, userId: user._id });
         });
+    });
+});
+
+// role 0 = 일반유저, 0이 아니면 관리자
+
+//auth는 미들웨어 = '/api/users/auth' 와 (req,res) 중간에서 무언가를 해주는 역할
+app.get("/api/users/auth", auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        Image: req.user.image,
     });
 });
 
