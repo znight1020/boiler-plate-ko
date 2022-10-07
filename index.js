85c9a2c4b8e6c8b58dae8131ddad3174c958ba63
현수 <104980470+znight1020@users.noreply.github.com>
2022-10-07 11:13:23 +0900

    apply nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -11,7 +19,21 @@ mongoose
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! 해치웠나 굿");
+});
+
+app.post("/register", (req, res) => {
+    //회원 가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
 });
 
 app.listen(port, () => {
