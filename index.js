5dc9ddf0f93ea9bc0e216e794e50f1175ccd0194
현수 <104980470+znight1020@users.noreply.github.com>
2022-10-08 07:25:19 +0900

    login기능 및 token생성

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const { User } = require("./models/User");
@@ -12,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -39,17 +41,36 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     // 요청된 이메일을 데이터베이스에 있는지 찾는다.
-    User.findOne({ email: req.body.email }, (err, userInfo) => {
-        if (!userInfo) {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
             return res.json({
                 loginSuccess: false,
                 message: "제공된 이메일에 해당하는 유저가 없습니다.",
             });
         }
+
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인한다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다.",
+                });
+        });
+
+        // 비밀번호까지 맞다면 토큰을 생성해준다. 토큰생성을 위해 JSONWEBTOKEN 라이브러리를 다운받아야 함
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err); // 400은 실패했다는 뜻
+
+            // 토큰을 저장한다. 어디에 ?  쿠기, 로컬스토리지 등등 저장할 곳은 많음,
+            // 어디가 가장 안전한가? 논란이 많다, 각자 다 장단점이 있음
+
+            //쿠키에 저장해보자
+            res.cookie("x_auth", user.token)
+                .status(200) // 200은 성공했다는 뜻
+                .json({ loginSuccess: true, userId: user._id });
+        });
     });
-    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인한다.
-    User.comparePassword(req.body.password, (err, isMatch) => {});
-    // 비밀번호까지 맞다면 토큰을 생성해준다.
 });
 
 app.listen(port, () => {
