b67dfc9c62c1c2c07ae38460a0f53dd8397de28c
현수 <104980470+znight1020@users.noreply.github.com>
2022-10-10 01:06:01 +0900

    server와 client 분할 및 react 설치

--- server/index.js
+++ server/index.js
@@ -91,6 +91,7 @@ app.get("/api/users/auth", auth, (req, res) => {
 });
 
 app.get("/api/users/logout", auth, (req, res) => {
+    // 로그아웃 기능
     User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
